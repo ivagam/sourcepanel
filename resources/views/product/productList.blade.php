87d6b3d086f13d0e92a94fc7cc7f9277ab6796da
saravananm1996 <160225112+saravananm1996@users.noreply.github.com>
2025-07-10 17:02:43 +0530

    product list correction

--- resources/views/product/productList.blade.php
+++ resources/views/product/productList.blade.php
@@ -4,11 +4,7 @@
     $subTitle = 'Product Grid';
     $script = '<script>
 
-        let table = new DataTable("#dataTable");
-
-        $(".remove-item-btn").on("click", function() {
-            $(this).closest("tr").addClass("d-none")
-        });
+        let table = new DataTable("#dataTable");       
     </script>';
 @endphp
 
@@ -26,30 +22,21 @@
         </div>
         <div class="card-body">
             
-            <table class="table bordered-table sm-table mb-0" id="dataTable" data-page-length='10'>
+            <div class="table-responsive">
+        <table class="table bordered-table mb-0" id="dataTable" style="min-width: 1000px;">
                 <thead>
                     <tr>
-                        <th class="text-center">S.L</th>
-                        <th class="text-center">Product Name</th>
-                        <th class="text-center">Category</th>
-                        <th class="text-center">Product Price</th>
-                        <th class="text-center">Description</th>
-                        <th class="text-center">Action</th>
+                        <th class="text-center text-nowrap" style="width: 10%;">Action</th>
+                        <th class="text-center" style="width: 20%;">Product Name</th>
+                        <th class="text-center" style="width: 20%;">Category</th>
+                        <th class="text-center" style="width: 15%;">Product Price</th>
+                        <th class="text-center" style="width: 30%;">Description</th>                        
                     </tr>
                 </thead>
                 <tbody>
                     @forelse ($products as $key => $product)
                         <tr>
-                            <td>
-                                    <div class="d-flex align-items-center gap-10">                                    
-                                        {{ $key + 1 }}
-                                    </div>
-                            </td>
-                            <td>{{ $product->product_name }}</td>
-                            <td>{{ $product->category_name }}</td>
-                            <td>{{ $product->product_price }}</td>
-                            <td>{{ $product->description }}</td>
-                            <td class="text-center">
+                           <td class="text-center">
                                 <div class="d-flex align-items-center gap-10 justify-content-center">
                                     <a href="{{ route('editProduct', $product->product_id) }}">
                                         <button type="button" class="bg-success-focus text-success-600 bg-hover-success-200 fw-medium w-40-px h-40-px d-flex justify-content-center align-items-center rounded-circle">
@@ -65,6 +52,10 @@
                                     </form>
                                 </div>
                             </td>
+                            <td>{{ $product->product_name }}</td>
+                            <td>{{ $product->category_name }}</td>
+                            <td>{{ $product->product_price }}</td>
+                            <td>{{ $product->description }}</td>                            
                         </tr>
                     @empty
                         <tr>
@@ -73,6 +64,7 @@
                     @endforelse
                 </tbody>
             </table>                                  
+        </div>
 
         </div>
     </div>
@@ -83,4 +75,21 @@
     setTimeout(function() {
         $(".alert").fadeOut("slow");
     }, 3000);
+
+    $(document).on('click', '.remove-item-btn', function() {
+    let form = $(this).closest('form');
+    if (confirm('Are you sure you want to delete this product?')) {
+        $.ajax({
+            url: form.attr('action'),
+            type: 'POST',
+            data: form.serialize(),
+            success: function(response) {
+                form.closest('tr').fadeOut();
+            },
+            error: function() {
+                alert('Failed to delete. Please try again.');
+            }
+        });
+    }
+});
 </script>
